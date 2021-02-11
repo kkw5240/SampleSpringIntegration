3651db77919bd0081c050ead982a61ceca32eb64
kkw5240 <kkw5240@gmail.com>
2021-02-11 15:54:26 +0900

    Initial commit

---
+++ src/main/java/com/example/kwkim/SampleSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kwkim/SampleSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
